76c3efc630d6993d7023d065e4e77269a559b956
cauavorpagel <cauamvorpagel@gmail>
2022-12-16 20:40:03 -0300

    06/12

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function cardCreate(name, product, originalPrice, newPrice) {
     return`
         <div class="swiper-slide">
             <div class="card">
-            <p>${name}</p><img class="imgproduct" src="./img/${product}.jfif" alt=""><span>De ${originalPrice}</span><span>Por ${newPrice}<span>
+            <p>${name}</p><img class="imgproduct" src="./img/dayProducts/img-${product}.jfif" alt=""><span>De ${originalPrice}</span><span>Por ${newPrice}<span>
             </div>
         </div>
     `
@@ -10,11 +10,8 @@ function cardCreate(name, product, originalPrice, newPrice) {
 
 document.querySelector(".swiper-wrapper").innerHTML = `            
     
-    ${cardCreate("Pão", "paoteste", "R$4,99", "R$2,99")}
-    ${cardCreate("Pão", "paoteste", "R$4,99", "R$2,99")}
-    ${cardCreate("Pão", "paoteste", "R$4,99", "R$2,99")}
-    ${cardCreate("Pão", "paoteste", "R$4,99", "R$2,99")}  
-    ${cardCreate("Pão", "paoteste", "R$4,99", "R$2,99")}  
-    ${cardCreate("Pão", "paoteste", "R$4,99", "R$2,99")}               
-                
+    ${cardCreate("Pastél", "pastel", "R$7,99", "R$4,99")}
+    ${cardCreate("Pão", "bread", "R$4,99", "R$2,99")}
+    ${cardCreate("Bolo de chocolate", "chocolateCake", "R$29,99", "R$15,99")}
+    ${cardCreate("Risóles de Frango", "risólesFrango", "R$10,99", "R$7,99")}  
 `
